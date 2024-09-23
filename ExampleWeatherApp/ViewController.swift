91bff477f5f489f3ca31e159e82a538f8d8a74d4
yonemoto <x-na-yonemoto@kddi-tech.com>
2024-09-24 00:58:00 +0900

    Initial Commit

---
+++ ExampleWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleWeatherApp
+//
+//  Created by 米本直樹 on 2024/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
