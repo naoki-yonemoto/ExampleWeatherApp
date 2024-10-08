65dba999b7a2cefc489e4d78ad403c5d1e3abb7b
yonemoto <x-na-yonemoto@kddi-tech.com>
2024-10-08 18:30:31 +0900

    commit

---
+++ ExampleWeatherApp/ExampleWeatherApp.swift
@@ -0,0 +1,19 @@
+//
+//  ExampleWeatherApp.swift
+//  ExampleWeatherApp
+//
+//  Created by 米本直樹 on 2024/10/08.
+//
+
+import SwiftUI
+
+//SwiftUI EntryPoint
+//MainActivityからComposeUIを起動するようなもの
+@main
+struct ExampleWeatherApp: App {    
+        var body: some Scene {
+            WindowGroup {
+                MainScreen()
+            }
+        }
+}
